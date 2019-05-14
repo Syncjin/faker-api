c5dc119bd670860383197d3298efe38e96916285
leewoojin <dldndldms@gmail.com>
2019-05-14 13:40:44 +0900

    git ignore

---
+++ fakedata.js
@@ -0,0 +1,165 @@
+var faker = require('faker');
+
+var result = {
+  cats: [],
+  imageUrl: ['http://localhost:3000/api/image/cat.jpg',
+  'http://localhost:3000/api/image/1_mONNI1lG9VuiqovpnYqicA.jpeg',
+  'http://localhost:3000/api/image/02-cat-training-NationalGeographic_1484324.jpg',
+  'http://localhost:3000/api/image/A-fluffy-cat-looking-funny-surprised-or-concerned.jpg',
+  'http://localhost:3000/api/image/cat2.jpeg',
+  'http://localhost:3000/api/image/Cat03.jpg',
+  'http://localhost:3000/api/image/cat4.jpeg',
+  'http://localhost:3000/api/image/cat5.jpeg',
+  'http://localhost:3000/api/image/cat6.jpeg',
+  'http://localhost:3000/api/image/cat7.jpg',
+  'http://localhost:3000/api/image/cat8.jpeg',
+  'http://localhost:3000/api/image/June_odd-eyed-cat_cropped.jpg',
+  'http://localhost:3000/api/image/photo-1518791841217-8f162f1e1131.jpeg',
+  'http://localhost:3000/api/image/This-Is-Why-Cats-Are-Afraid-of-Cucumbers-760x506.jpg',
+  'http://localhost:3000/api/image/what-does-it-mean-when-cat-wags-tail.jpg',],
+  imagePreLoadUrl: [
+    'http://localhost:3000/api/image/cat_pre.jpg',
+    'http://localhost:3000/api/image/1_mONNI1lG9VuiqovpnYqicA_pre.jpg',
+    'http://localhost:3000/api/image/02-cat-training-NationalGeographic_1484324_pre.jpg',
+    'http://localhost:3000/api/image/A-fluffy-cat-looking-funny-surprised-or-concerned_pre.jpg',
+    'http://localhost:3000/api/image/cat2_pre.jpg',
+    'http://localhost:3000/api/image/Cat03_pre.jpg',
+    'http://localhost:3000/api/image/cat4_pre.jpg',
+    'http://localhost:3000/api/image/cat5_pre.jpg',
+    'http://localhost:3000/api/image/cat6_pre.jpg',
+    'http://localhost:3000/api/image/cat7_pre.jpg',
+    'http://localhost:3000/api/image/cat8_pre.jpg',
+    'http://localhost:3000/api/image/June_odd-eyed-cat_cropped_pre.jpg',
+    'http://localhost:3000/api/image/photo-1518791841217-8f162f1e1131_pre.jpg',
+    'http://localhost:3000/api/image/This-Is-Why-Cats-Are-Afraid-of-Cucumbers-760x506_pre.jpg',
+    'http://localhost:3000/api/image/what-does-it-mean-when-cat-wags-tail_pre.jpg',
+  ],
+  getRandom: function(max, min){ return Math.floor(Math.random() * (max - min + 1)) + min},
+  makeFake: function() {
+    const max = 1000;
+    const min = 500;
+    for(let i = 1; i < 501; i++){
+      let width = this.getRandom(max, min);
+      let height = this.getRandom(max, min);
+      this.cats[i - 1] = {
+        id: i,
+        key: i,
+        name: faker.name.firstName(),
+        created_at: faker.date.past(),
+        urls: { 
+          small: this.imageUrl[(i - 1) % 15],
+          preLoad: this.imagePreLoadUrl[(i - 1) % 15]
+        },
+        width,
+        height,
+        views: faker.random.number(),
+        likes: faker.random.number(),
+        downloads: faker.random.number(),
+        alt_description: faker.lorem.word(),
+        description: faker.lorem.words(),
+      }
+    }
+  },
+  findFake: function(id) {
+    if(id){
+      // console.log(cats.cats)
+      // console.log(cats.cats[id])
+      return this.cats[id]
+      // for(let i = 0, len = cats.cats.length; i < len; i++){
+      //   if
+      // }
+      // return cats.cats[id];
+    }
+    console.log('cats?', this.cats)
+    return this.cats;
+  }
+}
+
+result.makeFake();
+// var cats = [];
+
+// getRandom = (max, min) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+// var imageUrl = [
+//   'http://localhost:3000/api/image/cat.jpg',
+//   'http://localhost:3000/api/image/1_mONNI1lG9VuiqovpnYqicA.jpeg',
+//   'http://localhost:3000/api/image/02-cat-training-NationalGeographic_1484324.jpg',
+//   'http://localhost:3000/api/image/A-fluffy-cat-looking-funny-surprised-or-concerned.jpg',
+//   'http://localhost:3000/api/image/cat2.jpeg',
+//   'http://localhost:3000/api/image/Cat03.jpg',
+//   'http://localhost:3000/api/image/cat4.jpeg',
+//   'http://localhost:3000/api/image/cat5.jpeg',
+//   'http://localhost:3000/api/image/cat6.jpeg',
+//   'http://localhost:3000/api/image/cat7.jpg',
+//   'http://localhost:3000/api/image/cat8.jpeg',
+//   'http://localhost:3000/api/image/June_odd-eyed-cat_cropped.jpg',
+//   'http://localhost:3000/api/image/photo-1518791841217-8f162f1e1131.jpeg',
+//   'http://localhost:3000/api/image/This-Is-Why-Cats-Are-Afraid-of-Cucumbers-760x506.jpg',
+//   'http://localhost:3000/api/image/what-does-it-mean-when-cat-wags-tail.jpg',
+// ]
+
+// var imagePreLoadUrl = [
+//   'http://localhost:3000/api/image/cat_pre.jpg',
+//   'http://localhost:3000/api/image/1_mONNI1lG9VuiqovpnYqicA_pre.jpg',
+//   'http://localhost:3000/api/image/02-cat-training-NationalGeographic_1484324_pre.jpg',
+//   'http://localhost:3000/api/image/A-fluffy-cat-looking-funny-surprised-or-concerned_pre.jpg',
+//   'http://localhost:3000/api/image/cat2_pre.jpg',
+//   'http://localhost:3000/api/image/Cat03_pre.jpg',
+//   'http://localhost:3000/api/image/cat4_pre.jpg',
+//   'http://localhost:3000/api/image/cat5_pre.jpg',
+//   'http://localhost:3000/api/image/cat6_pre.jpg',
+//   'http://localhost:3000/api/image/cat7_pre.jpg',
+//   'http://localhost:3000/api/image/cat8_pre.jpg',
+//   'http://localhost:3000/api/image/June_odd-eyed-cat_cropped_pre.jpg',
+//   'http://localhost:3000/api/image/photo-1518791841217-8f162f1e1131_pre.jpg',
+//   'http://localhost:3000/api/image/This-Is-Why-Cats-Are-Afraid-of-Cucumbers-760x506_pre.jpg',
+//   'http://localhost:3000/api/image/what-does-it-mean-when-cat-wags-tail_pre.jpg',
+// ]
+
+// makeFake = (cats) => {
+//   const max = 1000;
+//   const min = 500;
+//   for(let i = 1; i < 201; i++){
+//     let width = getRandom(max, min);
+//     let height = getRandom(max, min);
+//     cats[i] = {
+//       id: i,
+//       key: i,
+//       name: faker.name.firstName(),
+//       created_at: faker.date.past(),
+//       urls: { 
+//         small: imageUrl[(i - 1) % 15],
+//         // small: faker.random.image(),
+//         preLoad: imagePreLoadUrl[(i - 1) % 15]
+//       },
+//       width,
+//       height,
+//       views: faker.random.number(),
+//       likes: faker.random.number(),
+//       downloads: faker.random.number(),
+//       alt_description: faker.lorem.word(),
+//       description: faker.lorem.words(),
+//     }
+//   }
+
+//   return {
+//     "cats": cats
+//   }
+// }
+
+// cats = makeFake(cats);
+// console.log('cats', cats.cats[0])
+// module.exports = function(id) {
+//   console.log('find?', id)
+//   if(id){
+//     console.log(cats.cats)
+//     console.log(cats.cats[id])
+//     // for(let i = 0, len = cats.cats.length; i < len; i++){
+//     //   if
+//     // }
+//     return cats.cats[id];
+//   }
+//   return cats;
+// }
+
+module.exports = result;
---
+++ routes/index.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var fakedata = require('../fakedata');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* FAKER API */
+router.get('/api/card', (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+
+  let count = req.query.count;
+  let page = req.query.page;
+  let id = req.query.id;
+  let data = fakedata.cats;
+
+  setTimeout(() => {
+    
+    if(id){
+      res.send(data[id-1])
+    } else if(count !== undefined && page !== undefined){
+      count = Number(count);
+      page = Number(page);
+      console.log('data',count*page, count * (page + 1))
+      data = data.slice(count*page, count * (page + 1));
+      res.send(data)
+    } else if(count !== undefined){
+      count = Number(count);
+      data = data.slice(0,  count);
+      res.send(data)
+    } else {
+      res.send(data)
+    }
+  }, 1000);
+});
+
+router.get('/api/image/:image', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', '/images/', req.params.image));
+})
+
+module.exports = router;
